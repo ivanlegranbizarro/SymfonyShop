d875950941182893992e35c77c6cf7203d37458d
Iván Legrán <ivanlegranbizarro@gmail.com>
2025-05-28 09:48:24 +0200

    productsFilteredByCategory

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, ProductRepository $productRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $query = $request->query->get('q');
+        $categoryId = $request->query->getInt('categoryId') ?: null;
+
+        $products = $productRepository->findBySearchAndCategory($query, $categoryId);
+
+        return $this->render('home/index.html.twig', [
+            'products' => $products,
+        ]);
     }
 }
